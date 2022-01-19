96d67d0e24c7933fbb8b042c8a758bd3c6f2658b
Chris Kelley <chriskelley85@gmail.com>
2022-01-19 15:11:58 -0600

    added action,reducer, data, component folders

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import './App.css';
+import AuthorList from './components/AuthorList';
+import SelectedQuote from './components/SelectedQuote';
+import NavBar from './components/NavBar';
+
 
 function App() {
   return (
     <div className="App">
-      Async Redux Project
+      <section className='quotes'>
+        <SelectedQuote />
+        <AuthorList />
+        </section>
     </div>
   );
 }
 
-export default App;
+export default connect()(App);
